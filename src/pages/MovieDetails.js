4f3acff1724119ae9260d8a1f3d1964339173dbc
Tulio <tuliotrindade99@gmail.com>
2021-07-06 17:29:14 -0300

    requisito 4 corrigido

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
-import PropTypes from 'prop-types';
 
 class MovieDetails extends Component {
   constructor() {
